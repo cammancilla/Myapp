8540e92fe6452826def6426842f8478ec331420c
cammancilla <cam.mancilla@duocuc.cl>
2025-01-15 16:49:12 -0300

    avance en 3° clase
    nota: terminar todos los apartados pendientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {Component} from '@angular/core';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    MatProgressSpinnerModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideAnimationsAsync(),
+  ],
+    
   bootstrap: [AppComponent],
+ 
 })
 export class AppModule {}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -13,7 +14,7 @@ export class HomePage {
   };
 
   msj = '';
-  constructor(private router: Router) {}
+  constructor(private router: Router, private animation: AnimationController) {}
 
   conectar() {
     if (this.user.usuario.length > 0 && this.user.password.length > 0) {
@@ -24,6 +25,30 @@ export class HomePage {
     } else {
       this.msj = 'Credenciales no pueden estar vacias';
     }
+  }
+
+  ngAfterContentInit(){
+    this.animacionLogin();
+  }
+
+  animacionLogin(){
+    const imagen = document.querySelector(
+      '#container ion-card ion-card-header ion-img'
+    ) as HTMLElement;
+
+    const animacion = this.animation
+    .create()
+    .addElement(imagen)
+    .duration(3000)
+    .iterations(Infinity)
+    .keyframes(
+      [
+        {offset:0, opacity:'1', border:'2px solid white', transform: 'translateX(0)'},
+        {offset:0.25, opacity:'0.5', border:'2px solid red', transform:'translateX(0)'},
+        {offset:1, opacity:'0.20', border:'2px solid green', transform:'translateX(0)'},
+      ]
+      )
+  animacion.play();  
   }
 }
 
